f4edaf2fb58d3137044d72502457eddf13c9a591
Ferdinand Colico <fj.colico18@gmail.com>
2026-08-16 10:54:38 +0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "mulish": ["Mulish"],
+        "barlow": ["Barlow"],
+        "barlow-cond": ["Barlow Condensed"],
+        "dm-mono": ["DM Mono"]
+      }
+    },
+  },
+  plugins: [],
+}
+
